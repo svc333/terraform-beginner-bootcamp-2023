b11db260fcaf582a8fcbaec00c52ea0cf6aa82e8
svc333 <36961991+svc333@users.noreply.github.com>
2023-10-04 21:45:24 -0400

    #12 GO (#15)

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
   }
 }
 
@@ -12,10 +16,20 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 63
   special          = false
+  lower            = true
+  upper            = false
 }
 
 output "random_bucket_name_result" {
   value = random_string.bucket_name.result
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    Environment = "test"
+  }
 }
