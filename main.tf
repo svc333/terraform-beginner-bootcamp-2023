f4817ae0b0cbcae9b6fc7f39bb6bd611568b717f
svc333 <godzmarine@gmail.com>
2023-10-08 14:03:19 +0000

    #32 refactor s3 bucket into the new terrahouse_aws_module

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Environment = "test"
-    UserUuid = "var.user_uuid"
-  }
+terraform {
+  
 }
 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
---
+++ modules/terrahouse_aws/resources.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = "test"
+    UserUuid = "var.user_uuid"
+  }
+}
